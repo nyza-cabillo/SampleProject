bd183cb880c05299499125a5e4547d98028b88e9
kheart171 <ckheart1965@gmail.com>
2025-03-06 15:44:31 +0800

    feat: updated layout and views

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,62 +1,36 @@
 <script setup>
-import { ref } from 'vue'
-
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+import AppLayout from '@/components/layout/AppLayout.vue'
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto">
+          <v-card class="mx-auto" elevation="24">
+            <v-card-title class="text-center">
+              <h3 class="font-weight-black text-center">Welcome to Sample Project</h3>
+              <p class="mb-5">Login Form</p>
+            </v-card-title>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="Email" variant="outlined"></v-text-field>
 
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto">
-              <v-card class="mx-auto" prepend-icon="mdi-account" subtitle="Login" elevation="24">
-                <template v-slot:title>
-                  <span class="font-weight-black">Welcome to Sample Project</span>
-                </template>
+                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field label="Email" variant="outlined"></v-text-field>
+                <v-btn class="mt-2" type="submit" block prepend-icon="mdi-login">Login</v-btn>
+              </v-form>
 
-                    <v-text-field
-                      label="Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
-
-                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-                  </v-form>
-
-                  <v-divider class="my-5"></v-divider>
-                  <h5 class="text-center">
-                    Don't have account?
-                    <RouterLink to="register"><u>Click here to Register!</u></RouterLink>
-                  </h5>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer border app>2025 - copyright sample</v-footer>
-    </v-app>
-  </v-responsive>
+              <v-divider class="my-5"></v-divider>
+              <h5 class="text-center">
+                Don't have account?
+                <RouterLink to="register"><u>Click here to Register!</u></RouterLink>
+              </h5>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,74 +1,49 @@
 <script setup>
-import { ref } from 'vue'
-
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+import AppLayout from '@/components/layout/AppLayout.vue'
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto">
+          <v-card
+            class="mx-auto"
+            prepend-icon="mdi-account-plus"
+            subtitle="Registration Form"
+            elevation="24"
+          >
+            <template v-slot:title>
+              <span class="font-weight-black">Welcome to Sample Project</span>
+            </template>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="Email" variant="outlined"></v-text-field>
+                <v-text-field label="First Name" variant="outlined"></v-text-field>
+                <v-text-field label="Last Name" variant="outlined"></v-text-field>
 
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto">
-              <v-card
-                class="mx-auto"
-                prepend-icon="mdi-account-plus"
-                subtitle="Register"
-                elevation="24"
-              >
-                <template v-slot:title>
-                  <span class="font-weight-black">Welcome to Sample Project</span>
-                </template>
+                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field label="Email" variant="outlined"></v-text-field>
-                    <v-text-field label="First Name" variant="outlined"></v-text-field>
-                    <v-text-field label="Last Name" variant="outlined"></v-text-field>
+                <v-text-field
+                  label="Confirm Password"
+                  type="password"
+                  variant="outlined"
+                ></v-text-field>
 
-                    <v-text-field
-                      label="Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
-
-                    <v-text-field
-                      label="Confirm Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
-
-                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-                  </v-form>
-                  <v-divider class="my-5"></v-divider>
-                  <h5 class="text-center">
-                    Already have account?
-                    <RouterLink to="login"><u>Click here to Login!</u></RouterLink>
-                  </h5>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer border app>2025 - copyright sample</v-footer>
-    </v-app>
-  </v-responsive>
+                <v-btn class="mt-2" type="submit" block prepend-icon="mdi-account-plus"
+                  >Register</v-btn
+                >
+              </v-form>
+              <v-divider class="my-5"></v-divider>
+              <h5 class="text-center">
+                Already have account?
+                <RouterLink to="login"><u>Click here to Login!</u></RouterLink>
+              </h5>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
